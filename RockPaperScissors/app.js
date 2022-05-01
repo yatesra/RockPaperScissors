d614b45c67b6f1b634bbcaaa2b24f32eabb1d743
Rochelle <54810923+yatesra@users.noreply.github.com>
2022-04-30 23:55:29 -0400

    Update app.js
    changes I could not figure out how to add from git or github desktop

--- RockPaperScissors/app.js
+++ RockPaperScissors/app.js
@@ -9,3 +9,43 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
     userChoiceDisplay = e.target.id
     userChoiceDisplay.innerHTML = userChoiceDisplay
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3) + 1 // commenting just to practice commenting
+
+if (randomNumber === 1) {
+    computerChoice = 'rock'
+}
+if (randomNumber === 2) {
+    computerChoice = 'scissors'
+}
+if (randomNumber === 3) { 
+    computerChoice = 'paper'
+}
+computerChoiceDisplay.innerHTML = computerChoice
+}
+
+function getResult() {
+    if (computerChoice === userChoice) {
+        result = 'its a draw!'
+    }
+    if (computerChoice === 'rock' && userChoice === "paper") {
+        result = 'you win!'
+    }
+    if (computerChoice === 'rock' && userChoice === "scissors") {
+        result = 'you lose!'
+    }
+    if (computerChoice === 'paper' && userChoice === "scissors") {
+        result = 'you win!'
+    } 
+    if (computerChoice === 'paper' && userChoice === "rock") {
+        result = 'you lose!'
+    }
+    if (computerChoice === 'scissors' && userChoice === "rock") {
+        result = 'you win!'
+    }
+    if (computerChoice === 'scissors' && userChoice === "paper") {
+        result = 'you lose!'
+    }
+    resultDisplay.innerHTML = result
+}
